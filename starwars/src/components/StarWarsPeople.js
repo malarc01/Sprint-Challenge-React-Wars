99b9239314d0e5fae591e9692f83bf29e2579230
Husot <cobalt5alcon@gmail.com>
2019-03-09 13:37:52 -0500

    added css to StarWarsPeople.js&mvp

--- starwars/src/components/StarWarsPeople.js
+++ starwars/src/components/StarWarsPeople.js
@@ -4,12 +4,12 @@ import './StarWars.css'
 function StarWarsPeople(props){
   return(
     <div className="starwars-info">
-    <h3>X</h3>
-    <h3>{props.person.name} </h3>
-    <h3>{props.person.gender} </h3>
-    <h3>{props.person.mass} </h3>
-    <h3>{props.person.eye_color} </h3>
-    <h3>{props.person.birth_year} </h3>
+    <h3>Character Info</h3>
+    <h3> I am {props.person.name} </h3>
+    <h3>Gender: {props.person.gender} </h3>
+    <h3>Weigh: {props.person.mass} </h3>
+    <h3>Eye Color: {props.person.eye_color} </h3>
+    <h3>Year of Birth: {props.person.birth_year} </h3>
 
     </div>
   )
